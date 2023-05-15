f37f35842d3708e683a87e11441ce478dde62fc9
Felipe Scherer <felipescherer25@gmail.com>
2023-05-15 12:49:09 -0300

    :sparkles: feat: header ignite feed

--- rocketseat/rockeseat-bootcamp/reactjs/01-ignite-feed/src/App.jsx
+++ rocketseat/rockeseat-bootcamp/reactjs/01-ignite-feed/src/App.jsx
@@ -2,12 +2,14 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 
 import "./global.css";
+import { Header } from "./components/Header";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div>
+      <Header></Header>
       <div>dasds</div>
     </div>
   );
--- rocketseat/rockeseat-bootcamp/reactjs/01-ignite-feed/src/components/Header/index.jsx
+++ rocketseat/rockeseat-bootcamp/reactjs/01-ignite-feed/src/components/Header/index.jsx
@@ -1,8 +1,10 @@
 import styles from "./Header.module.css";
+import igniteLogo from "../../assets/logo.svg";
 
 export function Header() {
   return (
-    <div>
+    <div className={styles.header}>
+      <img src={igniteLogo} alt="Logo" />
       <strong className={styles.Header}>Ignite Feed</strong>
     </div>
   );
