52cb0eaa4f7d90d1ec85d4835b295de010e6459d
Felipe Scherer <felipescherer25@gmail.com>
2023-05-30 21:11:25 -0300

    :sparkles: avatar component

--- rocketseat/rockeseat-bootcamp/reactjs/01-ignite-feed/src/components/Sidebar/index.jsx
+++ rocketseat/rockeseat-bootcamp/reactjs/01-ignite-feed/src/components/Sidebar/index.jsx
@@ -1,16 +1,15 @@
 import styles from "./Sidebar.module.css";
 import image from "../../assets/teste.png";
-import {PencilLine} from '@phosphor-icons/react'
+import { PencilLine } from "@phosphor-icons/react";
+import { Avatar } from "../Avatar";
 
 export function Sidebar() {
   return (
     <aside className={styles.sidebar}>
       <img src={image} />
 
-
       <div className={styles.profile}>
-
-      <img className={styles.avatar} src="https://github.com/ofelipescherer.png" alt="Profile image" />
+        <Avatar src="https://github.com/ofelipescherer.png" />
 
         <span>Felipe Scherer</span>
         <span>Developer</span>
@@ -19,7 +18,8 @@ export function Sidebar() {
       <footer>
         <a href="">
           <PencilLine size={20} />
-          Editar seu perfil</a>
+          Editar seu perfil
+        </a>
       </footer>
     </aside>
   );
