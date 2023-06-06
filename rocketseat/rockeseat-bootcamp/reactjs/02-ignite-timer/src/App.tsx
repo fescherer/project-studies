93cc181dc876dd05c55c6a1ee9d0aafe20334737
Felipe Scherer <felipescherer25@gmail.com>
2023-06-06 20:22:36 -0300

    :sparkles: styled component

--- rocketseat/rockeseat-bootcamp/reactjs/02-ignite-timer/src/App.tsx
+++ rocketseat/rockeseat-bootcamp/reactjs/02-ignite-timer/src/App.tsx
@@ -1,10 +1,14 @@
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './theme/theme'
+import { GlobalStyle } from './global'
+
 function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <h2>hello world</h2>
-    </>
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
 
 export default App
-
