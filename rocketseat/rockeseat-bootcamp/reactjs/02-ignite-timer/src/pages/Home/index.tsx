548b9898e9ef915b93cc974c6480311a0cea8f37
Felipe Scherer <felipescherer25@gmail.com>
2023-06-09 13:57:23 -0300

    :sparkles: feat: typescript

--- rocketseat/rockeseat-bootcamp/reactjs/02-ignite-timer/src/pages/Home/index.tsx
+++ rocketseat/rockeseat-bootcamp/reactjs/02-ignite-timer/src/pages/Home/index.tsx
@@ -13,12 +13,14 @@ const newCycleFormValidationSchema = zod.object({
     .max(60, 'O ciclo precisa ser de no máximo 60 minutos.')
 })
 
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+
 export function Home() {
-  const { register, handleSubmit, watch } = useForm({
+  const { register, handleSubmit, watch } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema)
   })
 
-  function handleCreateNewCycle(data: any) {
+  function handleCreateNewCycle(data: NewCycleFormData) {
     console.log(data)
   }
 
