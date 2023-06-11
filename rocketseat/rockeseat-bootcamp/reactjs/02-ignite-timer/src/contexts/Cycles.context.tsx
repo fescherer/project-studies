1a4e19c25d0c92ce0e7fa31773ea35810cb90ef5
Felipe Scherer <felipescherer25@gmail.com>
2023-06-11 17:24:57 -0300

    :sparkles: feat: save localstorage

--- rocketseat/rockeseat-bootcamp/reactjs/02-ignite-timer/src/contexts/Cycles.context.tsx
+++ rocketseat/rockeseat-bootcamp/reactjs/02-ignite-timer/src/contexts/Cycles.context.tsx
@@ -1,4 +1,10 @@
-import { createContext, ReactNode, useReducer, useState } from 'react'
+import {
+  createContext,
+  ReactNode,
+  useEffect,
+  useReducer,
+  useState
+} from 'react'
 import { Cycle } from '../@types/Cycle'
 import {
   addNewCycleAction,
@@ -6,6 +12,7 @@ import {
   markCurrentCycleAsFinishedAction
 } from '../reducers/cycles/actions'
 import { cyclesReducer } from '../reducers/cycles/reducer'
+import { differenceInSeconds } from 'date-fns'
 
 type CreateCycleData = {
   task: string
@@ -32,16 +39,38 @@ interface CyclesContextProviderProps {
 export function CyclesContextProvider({
   children
 }: CyclesContextProviderProps) {
-  const [cyclesState, dispatch] = useReducer(cyclesReducer, {
-    cycles: [],
-    activeCycleId: null
-  })
+  const [cyclesState, dispatch] = useReducer(
+    cyclesReducer,
+    {
+      cycles: [],
+      activeCycleId: null
+    },
+    (initialState) => {
+      const storageStateAsJSON = localStorage.getItem(
+        '@ignite-timer:cycles-state-1.0.0'
+      )
 
-  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
+      if (storageStateAsJSON) return JSON.parse(storageStateAsJSON)
+      return initialState
+    }
+  )
 
   const { cycles, activeCycleId } = cyclesState
 
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
+
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(() => {
+    if (activeCycle) {
+      return differenceInSeconds(new Date(), new Date(activeCycle.startDate))
+    }
+    return 0
+  })
+
+  useEffect(() => {
+    const stateJSON = JSON.stringify(cyclesState)
+
+    localStorage.setItem('@ignite-timer:cycles-state-1.0.0', stateJSON)
+  }, [cyclesState])
 
   function setSecondsPassed(seconds: number) {
     setAmountSecondsPassed(seconds)
