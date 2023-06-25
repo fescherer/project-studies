2a298886fa249e9847a2b62953f376332dffed6a
Felipe Scherer <felipescherer25@gmail.com>
2023-06-25 16:46:20 -0300

    :sparkles: feat: dialog]

--- rocketseat/rockeseat-bootcamp/reactjs/03-DT-money/src/components/Header/index.tsx
+++ rocketseat/rockeseat-bootcamp/reactjs/03-DT-money/src/components/Header/index.tsx
@@ -1,6 +1,8 @@
 import * as S from './styles'
+import * as Dialog from '@radix-ui/react-dialog'
 
 import logoImg from '../../assets/header-logo.svg'
+import { NewTransactionModal } from '../NewTransactionModal'
 
 export function Header() {
   return (
@@ -8,7 +10,13 @@ export function Header() {
       <S.HeaderContent>
         <img src={logoImg} alt="" />
 
-        <S.NewTransactionButton>Nova transação</S.NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <S.NewTransactionButton>Nova transação</S.NewTransactionButton>
+          </Dialog.Trigger>
+
+          <NewTransactionModal />
+        </Dialog.Root>
       </S.HeaderContent>
     </S.HeaderContainer>
   )
