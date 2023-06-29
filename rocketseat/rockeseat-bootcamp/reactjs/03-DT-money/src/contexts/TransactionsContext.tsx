58058e279ec3e55cb35258bdbf071a9b59fd3051
Felipe Scherer <felipescherer25@gmail.com>
2023-06-29 12:59:28 -0300

    :sparkles: feat: context

---
+++ rocketseat/rockeseat-bootcamp/reactjs/03-DT-money/src/contexts/TransactionsContext.tsx
@@ -0,0 +1,33 @@
+import { createContext, ReactNode, useEffect, useState } from 'react'
+import { TransactionType } from '../@types/Transactions'
+
+interface TransactionContextType {
+  transactions: TransactionType[]
+}
+
+interface TransactionsProviderProps {
+  children: ReactNode
+}
+
+export const TransactionsContext = createContext({} as TransactionContextType)
+
+export function TransactionsProvider({ children }: TransactionsProviderProps) {
+  const [transactions, setTransactions] = useState<TransactionType[]>([])
+
+  async function loadTransactions() {
+    const response = await fetch('http://localhost:3333/transactions')
+    const data = await response.json()
+
+    setTransactions(data)
+  }
+
+  useEffect(() => {
+    loadTransactions()
+  }, [])
+
+  return (
+    <TransactionsContext.Provider value={{ transactions }}>
+      {children}
+    </TransactionsContext.Provider>
+  )
+}
--- rocketseat/rockeseat-bootcamp/reactjs/03-DT-money/src/pages/Transactions/index.tsx
+++ rocketseat/rockeseat-bootcamp/reactjs/03-DT-money/src/pages/Transactions/index.tsx
@@ -1,23 +1,12 @@
-import { useEffect, useState } from 'react'
+import { useContext } from 'react'
 import { Header } from '../../components/Header'
 import { Summary } from '../../components/Summary'
 import { SearchForm } from './components/SearchForm'
 import * as S from './styles'
-import { TransactionType } from '../../@types/Transactions'
+import { TransactionsContext } from '../../contexts/TransactionsContext'
 
 export function Transactions() {
-  const [transactions, setTransactions] = useState<TransactionType[]>([])
-
-  async function loadTransactions() {
-    const response = await fetch('http://localhost:3333/transactions')
-    const data = await response.json()
-
-    setTransactions(data)
-  }
-
-  useEffect(() => {
-    loadTransactions()
-  }, [])
+  const { transactions } = useContext(TransactionsContext)
 
   return (
     <div>
