e6e6608930c4196726ff9875bcf7a74cdc182c7b
Felipe Scherer <felipescherer25@gmail.com>
2023-07-02 22:01:00 -0300

    :sparkles: feat: arg types

--- rocketseat/rockeseat-bootcamp/reactjs/05-design-system/packages/docs/src/stories/Avatar.stories.tsx
+++ rocketseat/rockeseat-bootcamp/reactjs/05-design-system/packages/docs/src/stories/Avatar.stories.tsx
@@ -6,7 +6,14 @@ export default {
   component: Avatar,
   args: {
     src: 'https://github.com/ofelipescherer.png',
-    alt: 'Diego Fernandes'
+    alt: 'Felipe Scherer'
+  },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text'
+      }
+    }
   }
 } as Meta<AvatarProps>
 
