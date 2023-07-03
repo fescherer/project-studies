bb440ec403c5c149e747421f9675930e05b2936a
Felipe Scherer <felipescherer25@gmail.com>
2023-07-02 22:19:55 -0300

    :sparkles: storybook a11y

--- rocketseat/rockeseat-bootcamp/reactjs/05-design-system/packages/docs/.storybook/main.js
+++ rocketseat/rockeseat-bootcamp/reactjs/05-design-system/packages/docs/.storybook/main.js
@@ -7,14 +7,12 @@ const config = {
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@storybook/addon-interactions'
+    '@storybook/addon-interactions',
+    '@storybook/addon-a11y'
   ],
   framework: {
     name: '@storybook/react-vite',
     options: {}
-  },
-  docs: {
-    autodocs: 'tag'
   }
 }
 export default config
