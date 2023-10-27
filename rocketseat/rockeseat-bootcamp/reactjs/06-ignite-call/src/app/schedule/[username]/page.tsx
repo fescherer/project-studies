b55c3f2391305f06ddf196f4a63a6dd94687f93b
Felipe Scherer <felipescherer25@gmail.com>
2023-10-26 21:46:14 -0300

    :sparkles: feat: scheduler

---
+++ rocketseat/rockeseat-bootcamp/reactjs/06-ignite-call/src/app/schedule/[username]/page.tsx
@@ -0,0 +1,37 @@
+import { prisma } from '../../../lib/prisma'
+import Image from 'next/image'
+
+interface ScheduleProps {
+  params: {
+    username: string
+  }
+}
+
+export default async function Schedule({ params }: ScheduleProps) {
+  const user = await prisma.user.findUnique({
+    where: {
+      username: params.username,
+    },
+  })
+
+  if (!user) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return (
+    <div>
+      <div className="flex flex-col items-center">
+        <Image
+          alt="avatar"
+          width={40}
+          height={40}
+          src={user.avatar_url ?? ''}
+        />
+        <span>{user.name}</span>
+        <p>{user.bio}</p>
+      </div>
+    </div>
+  )
+}
