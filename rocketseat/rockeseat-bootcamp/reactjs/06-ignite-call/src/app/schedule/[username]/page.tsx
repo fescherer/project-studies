5fd2d406bb4e47221acbb6d7ca84333e839ed8f4
Felipe Scherer <felipescherer25@gmail.com>
2023-10-30 22:50:52 -0300

    :sparkles: style calendar'

--- rocketseat/rockeseat-bootcamp/reactjs/06-ignite-call/src/app/schedule/[username]/page.tsx
+++ rocketseat/rockeseat-bootcamp/reactjs/06-ignite-call/src/app/schedule/[username]/page.tsx
@@ -22,15 +22,16 @@ export default async function Schedule({ params }: ScheduleProps) {
   }
 
   return (
-    <div>
-      <div className="flex flex-col items-center">
+    <div className="m-auto flex max-w-md flex-col items-center gap-4 py-12">
+      <div className="flex flex-col items-center border-0">
         <Image
           alt="avatar"
-          width={40}
-          height={40}
+          width={80}
+          height={80}
+          className="border-ignite-500 rounded-full border"
           src={user.avatar_url ?? ''}
         />
-        <span>{user.name}</span>
+        <span className="text-white">{user.name}</span>
         <p>{user.bio}</p>
       </div>
 
--- rocketseat/rockeseat-bootcamp/reactjs/06-ignite-call/src/components/Calendar/index.tsx
+++ rocketseat/rockeseat-bootcamp/reactjs/06-ignite-call/src/components/Calendar/index.tsx
@@ -7,44 +7,52 @@ export function Calendar() {
   const shortWeekDays = getWeekDays({ short: true })
 
   return (
-    <div>
-      <div>
-        <span>
-          Dezembro <span>2022</span>
+    <div className="flex flex-col gap-6 p-6">
+      <div className="flex items-center justify-between">
+        <span className="font-medium">
+          Dezembro <span className="text-gray-200">2022</span>
         </span>
 
-        <div>
-          <button>
-            <CaretLeft />
+        <div className="flex gap-2 text-gray-200">
+          <button className="cursor-pointer rounded hover:text-gray-100">
+            <CaretLeft size={18} />
           </button>
-          <button>
-            <CaretRight />
+          <button className="cursor-pointer rounded hover:text-gray-100">
+            <CaretRight size={18} />
           </button>
         </div>
       </div>
 
-      <table>
+      <table className="w-full table-fixed border-spacing-1">
         <thead>
           <tr>
             {shortWeekDays.map((weekDay) => (
-              <th key={weekDay}>{weekDay}.</th>
+              <th className="text-sm text-gray-200" key={weekDay}>
+                {weekDay}.
+              </th>
             ))}
           </tr>
         </thead>
-        <tbody>
+        <tbody className='block leading-3 text-gray-800 before:content-["."]'>
           <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td>
-              <button>1</button>
+            <td className="box-border"></td>
+            <td className="box-border"></td>
+            <td className="box-border"></td>
+            <td className="box-border"></td>
+            <td className="box-border">
+              <button className="aspect-square w-full cursor-pointer rounded bg-gray-600 text-center disabled:cursor-default disabled:bg-none disabled:opacity-40 [&>:not(:disabled)]:bg-gray-600">
+                1
+              </button>
             </td>
-            <td>
-              <button>2</button>
+            <td className="box-border">
+              <button className="aspect-square w-full cursor-pointer rounded bg-gray-600 text-center disabled:cursor-default disabled:bg-none disabled:opacity-40 [&>:not(:disabled)]:bg-gray-600">
+                2
+              </button>
             </td>
-            <td>
-              <button>3</button>
+            <td className="box-border">
+              <button className="aspect-square w-full cursor-pointer rounded bg-gray-600 text-center disabled:cursor-default disabled:bg-none disabled:opacity-40 [&>:not(:disabled)]:bg-gray-600">
+                3
+              </button>
             </td>
           </tr>
         </tbody>
